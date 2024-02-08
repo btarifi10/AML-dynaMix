d7fea85b4ad91c45fadd87895032fca87624ece3
Basheq Tarifi <btarifi08@gmail.com>
2024-02-08 16:37:47 +0000

    Updates to app and client

--- src/DataCollectionApp.cpp
+++ src/DataCollectionApp.cpp
@@ -3,11 +3,19 @@
 #include "MAX30105.h"
 
 // Define constants
+int GSR_PIN = A0; // Analog pin for GSR readings
 int ECG_PIN = A1; // Analog pin for ECG readings
-int GSR_PIN = A2; // Analog pin for GSR readings
+int TEMP_PIN = A2; // Analog pin for temperature readings
 int RECORDING_DELAY_BUFFER = 10; // Delay between each recording in ms
 int START_INSTRUCTION = 1; 
 int STOP_INSTRUCTION = 2;
+
+int ADC_RESOLUTION = 4095;
+int ADC_REF_VOLTAGE = 3300; // in mV
+
+const int SAMPLE_WINDOW_SIZE = 4;
+
+const bool USE_GREEN = true;
 
 /*
 PPG Sensor Config
@@ -22,19 +30,41 @@ adcRange:      ADCRANGE_2048 ADCRANGE_4096 ADCRANGE_8192 ADCRANGE_16384
 */
 // DFRobot_MAX30102 PPG_SENSOR; // Connected via I2C
 MAX30105 PPG_SENSOR; // Connected via I2C
-byte LED_BRIGHTNESS = 0xFF; //Options: 0=Off to 255=50mA
-byte SAMPLE_AVERAGE = 4; //Options: 1, 2, 4, 8, 16, 32
-byte LED_MODE = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
-int SAMPLE_RATE = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
-int PULSE_WIDTH = 411; //Options: 69, 118, 215, 411
-int ADC_RANGE = 2048; //Options: 2048, 4096, 8192, 16384
 
 // Global state
 bool isRecording = false;
 bool connected = false;
 unsigned long startTime = 0;
 
+float ecgReadings[SAMPLE_WINDOW_SIZE];
+float gsrReadings[SAMPLE_WINDOW_SIZE];
+float ppgInfraredReadings[SAMPLE_WINDOW_SIZE];
+float ppgRedReadings[SAMPLE_WINDOW_SIZE];
+float ppgGreenReadings[SAMPLE_WINDOW_SIZE];
+float temperatureReadings[SAMPLE_WINDOW_SIZE];
+
+float sumECG = 0;
+float sumGSR = 0;
+float sumPPGInfrared = 0;
+float sumPPGRed = 0;
+float sumPPGGreen = 0;
+float sumTemperature = 0;
+
+int currentIndex = 0;
+
+float ecgAverage = 0;
+float gsrAverage = 0;
+float ppgInfraredAverage = 0;
+float ppgRedAverage = 0;
+float ppgGreenAverage = 0;
+float temperatureAverage = 0;
+
+float readAdcVoltage(int adcPin) {
+  return (analogRead(adcPin) * ADC_REF_VOLTAGE) / ADC_RESOLUTION;
+}
+
 void setup() {
+  // Initialise Serial
   Serial.begin(115200);
   while (!Serial);
   Serial.println("Initializing...");
@@ -45,10 +75,27 @@ void setup() {
     Serial.println("MAX30102 was not found. Please check wiring/power. ");
     while (1);
   }
+
+  // Initialise window buffers
+  for (int i = 0; i < SAMPLE_WINDOW_SIZE; i++) {
+    ecgReadings[i] = 0;
+    gsrReadings[i] = 0;
+    ppgInfraredReadings[i] = 0;
+    ppgRedReadings[i] = 0;
+    temperatureReadings[i] = 0;
+  }
+
   // PPG_SENSOR.sensorConfiguration();
   // PPG_SENSOR.setup();
-  PPG_SENSOR.setup(LED_BRIGHTNESS, SAMPLE_AVERAGE, LED_MODE, SAMPLE_RATE, PULSE_WIDTH, ADC_RANGE);
-  PPG_SENSOR.enableDIETEMPRDY();
+
+  byte LED_BRIGHTNESS = (byte) 60U; //Options: 0=Off to 255=50mA
+  byte SAMPLE_AVERAGE = (byte) 4U; //Options: 1, 2, 4, 8, 16, 32
+  byte LED_MODE = (byte) 2U; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte LED_MODE_GREEN = (byte) 3U; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  int SAMPLE_RATE = 400; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int PULSE_WIDTH = 215; //Options: 69, 118, 215, 411
+  int ADC_RANGE = 4096; //Options: 2048, 4096, 8192, 16384
+  PPG_SENSOR.setup(LED_BRIGHTNESS, SAMPLE_AVERAGE, USE_GREEN ? LED_MODE_GREEN : LED_MODE, SAMPLE_RATE, PULSE_WIDTH, ADC_RANGE);
   PPG_SENSOR.shutDown();
 
   Serial.println("Ready.");
@@ -71,22 +118,64 @@ void loop() {
   PPG_SENSOR.check();
   // Record data
   if (isRecording && PPG_SENSOR.available()) {
+
+    sumECG -= ecgReadings[currentIndex];
+    sumGSR -= gsrReadings[currentIndex];
+    sumTemperature -= temperatureReadings[currentIndex];
+    sumPPGInfrared -= ppgInfraredReadings[currentIndex];
+    sumPPGRed -= ppgRedReadings[currentIndex];
+
+    ecgReadings[currentIndex] = readAdcVoltage(ECG_PIN);
+    gsrReadings[currentIndex] = readAdcVoltage(GSR_PIN);
+    temperatureReadings[currentIndex] = readAdcVoltage(TEMP_PIN);
+    ppgInfraredReadings[currentIndex] = PPG_SENSOR.getIR();
+    ppgRedReadings[currentIndex] = PPG_SENSOR.getRed();
+
+    sumECG += ecgReadings[currentIndex];
+    sumGSR += gsrReadings[currentIndex];
+    sumTemperature += temperatureReadings[currentIndex];
+    sumPPGInfrared += ppgInfraredReadings[currentIndex];
+    sumPPGRed += ppgRedReadings[currentIndex];
+
+    if (USE_GREEN) {
+      sumPPGGreen -= ppgGreenReadings[currentIndex];
+      ppgGreenReadings[currentIndex] = PPG_SENSOR.getGreen();
+      sumPPGGreen += ppgGreenReadings[currentIndex];
+    }
+
+    currentIndex = (currentIndex + 1) % SAMPLE_WINDOW_SIZE;
+
+    ecgAverage = sumECG / SAMPLE_WINDOW_SIZE;
+    gsrAverage = sumGSR / SAMPLE_WINDOW_SIZE;
+    temperatureAverage = sumTemperature / SAMPLE_WINDOW_SIZE;
+    ppgInfraredAverage = sumPPGInfrared / SAMPLE_WINDOW_SIZE;
+    ppgRedAverage = sumPPGRed / SAMPLE_WINDOW_SIZE;
+
+    if (USE_GREEN) {
+      ppgGreenAverage = sumPPGGreen / SAMPLE_WINDOW_SIZE;
+    }
+
     // Record time
     Serial.print(millis() - startTime);
     Serial.print(" ");
     // Record ECG
-    Serial.print(analogRead(ECG_PIN));
+    Serial.print(ecgAverage);
     Serial.print(" ");
     // Record GSR
-    Serial.print(analogRead(GSR_PIN));
+    Serial.print(gsrAverage);
     Serial.print(" ");
     // Record PPG
-    Serial.print(PPG_SENSOR.getFIFOIR());
+    Serial.print(ppgInfraredAverage);
     Serial.print(" ");
-    Serial.print(PPG_SENSOR.getFIFORed());
+    Serial.print(ppgRedAverage);
     Serial.print(" ");
-    // Record temperature from PPG sensor
-    Serial.print(PPG_SENSOR.readTemperature());
+
+    if (USE_GREEN) {
+      Serial.print(ppgGreenAverage);
+      Serial.print(" ");
+    }
+
+    Serial.print(temperatureReadings[currentIndex]);
     Serial.print("\n");
 
     PPG_SENSOR.nextSample();
@@ -94,3 +183,4 @@ void loop() {
 
   delay(RECORDING_DELAY_BUFFER);
 }
+
--- src/DataCollectionClient.py
+++ src/DataCollectionClient.py
@@ -1,103 +1,147 @@
 from datetime import datetime
+from time import sleep
 import serial
 import json
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
+import threading
+import signal
+import sys
 
 # Initialize plot
 plt.ion()
-fig, ax = plt.subplots(1, 3)
-xdata, ydata = [], []
+fig, ax = plt.subplots(1, 5, figsize=(20, 5))
+titles = ['ECG', 'GSR', 'PPG Red', 'PPG IR', 'PPG Green']
+for axis, title in zip(ax, titles):
+    axis.set_title(title)
+    axis.set_xlabel('Time (s)')
+    axis.set_ylabel('Value')
+    axis.grid(True)
 
-plt.sca(ax[0])
-plot_ecg, = plt.plot([], [], 'r-')
-plt.sca(ax[1])
-plot_gsr, = plt.plot([], [], 'r-')
-plt.sca(ax[2])
-plot_ppg_red, = plt.plot([], [], 'r-')
-plot_ppg_ir, = plt.plot([], [], 'b-')
+# Creating separate plot lines for each measurement
+plot_lines = {
+    'ecg': ax[0].plot([], [], 'r-', label='ECG')[0],
+    'gsr': ax[1].plot([], [], 'g-', label='GSR')[0],
+    'ppg_red': ax[2].plot([], [], 'r-', label='PPG Red')[0],
+    'ppg_ir': ax[3].plot([], [], 'b-', label='PPG IR')[0],
+    'ppg_green': ax[4].plot([], [], 'g-', label='PPG Green')[0]
+}
 
-t, ecg, gsr, ppg_red, ppg_ir, temp = [], [], [], [], [], []
-data_list = []
+# Data lists
+t, ecg, gsr, ppg_red, ppg_ir, ppg_green, temp = [], [], [], [], [], [], []
+
+# Thread-safe mechanism for data updates
+data_lock = threading.Lock()
+
+# Flag to control the execution
+running = True
 
 # Function to update plot
-def update_plot(frame):
-    plot_ecg.set_data(t, ecg)
-    plot_gsr.set_data(t, gsr)
-    plot_ppg_red.set_data(t, ppg_red)
-    plot_ppg_ir.set_data(t, ppg_ir)
+def update_plot():
+    with data_lock:
+        window_size = 100  # Adjust window size for data viewing
+        if len(t) > window_size:
+            for line, data in zip(plot_lines.values(), [ecg, gsr, ppg_red, ppg_ir, ppg_green]):
+                line.set_data([tt / 1000 for tt in t[-window_size:]], data[-window_size:])
+        else:
+            for line, data in zip(plot_lines.values(), [ecg, gsr, ppg_red, ppg_ir, ppg_green]):
+                line.set_data([tt / 1000 for tt in t], data)
 
+    # Adjusting limits
     for axis in ax:
         axis.relim()
         axis.autoscale_view()
-    return plot_ecg, plot_gsr, plot_ppg_red, plot_ppg_ir
+    plt.draw()
 
-# Collect user details and format choice
-user_details = {
-    "name": input("Enter name: "),
-    "age": input("Enter age: "),
-    "sex": input("Enter sex: ")
-}
+# Signal handler for SIGINT
+def signal_handler(sig, frame):
+    global running
+    running = False
+    print("Terminating...")
 
-# Set up serial connection (adjust COM port as needed)
-baud_rate = 115200
-com_port = input("Enter COM port: ")
-ser = serial.Serial(com_port, baud_rate)
-ser.flush()
-device_found = False
-while not device_found:
-    if ser.in_waiting > 0:
-        message = ser.readline().decode('utf-8').rstrip()
-        if (message == "Ready."):
-            print(f"Found device on {com_port}, starting data collection...")
-            device_found = True
-        else:
-            print("Received message from device: ", message)
+# Register the signal handler
+signal.signal(signal.SIGINT, signal_handler)
 
-# Start data collection
-ser.write("Start\n".encode())
-# ser.flush()
-print("Data collection started. Press CTRL+C to stop.")
+# Function to collect data from the serial port
+def collect_data(ser):
+    global running
+    try:
+        while running:
+            if ser.in_waiting > 0:
+                sensor_data = ser.readline().decode('utf-8').rstrip()
+                timestamp_, ecg_, gsr_, ppg_ir_, ppg_red_, ppg_green_, temp_ = sensor_data.split(" ")
+                
+                with data_lock:
+                    t.append(float(timestamp_))
+                    ecg.append(float(ecg_))
+                    gsr.append(float(gsr_))
+                    ppg_red.append(float(ppg_red_))
+                    ppg_ir.append(float(ppg_ir_))
+                    ppg_green.append(float(ppg_green_))
+                    temp.append(float(temp_))
+    finally:
+        ser.close()
+        print("Serial port closed.")
 
-ani = FuncAnimation(fig, update_plot, blit=True, cache_frame_data=False)
-
-try:
-    while True:
-        if ser.in_waiting > 0:
-            sensor_data = ser.readline().decode('utf-8').rstrip()
-            # print(sensor_data)
-            timestamp_, ecg_, gsr_, ppg_ir_, ppg_red_, ppg_temp_ = sensor_data.split(" ")
-
-            t.append(float(timestamp_))
-            ecg.append(float(ecg_))
-            gsr.append(float(gsr_))
-            ppg_red.append(float(ppg_red_))
-            ppg_ir.append(float(ppg_ir_))
-            temp.append(float(ppg_temp_))
-
-            plt.pause(0.01)
-except KeyboardInterrupt:
-    ser.write("Stop\n".encode())
-finally:
-    ser.close()
-    print("Data collection stopped.")
-    plt.ioff()
-    plt.show()
-
-    # Format data
-    data = {
-        "user_details": user_details,
-        "timestamp": t,
-        "ecg": ecg,
-        "gsr": gsr,
-        "ppg_red": ppg_red,
-        "ppg_ir": ppg_ir,
-        "temp": temp
+if __name__ == "__main__":
+    # Collect user details
+    user_details = {
+        "name": input("Enter name: "),
+        "age": input("Enter age: "),
+        "sex": input("Enter sex: ")
     }
 
-    # name file with the users name and current time
-    time_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_name = f"{user_details['name']}_{time_now}.json"
+    # Set up serial connection
+    com_port = input("Enter COM port: ")
+    baud_rate = 115200
+    ser = serial.Serial(com_port, baud_rate)
+    ser.flush()
 
-    with open(file_name, "w") as data_file:
-        json.dump(data, data_file)
+    device_found = False
+    while not device_found:
+        if ser.in_waiting > 0:
+            message = ser.readline().decode('utf-8').rstrip()
+            if message == "Ready.":
+                print(f"Found device on {com_port}, starting data collection...")
+                device_found = True
+            else:
+                print("Received message from device: ", message)
+        else:
+            sleep(0.5)
+            print("Waiting for device...")
+
+    # Start data collection
+    ser.write("Start\n".encode())
+    print("Data collection started. Press CTRL+C to stop.")
+
+    # Start data collection thread
+    data_thread = threading.Thread(target=collect_data, args=(ser,))
+    data_thread.start()
+
+    try:
+        while running:
+            update_plot()
+            plt.pause(0.1)  # Adjust as necessary for update frequency
+    except KeyboardInterrupt:
+        print("Program interrupted by user.")
+    finally:
+        running = False
+        data_thread.join()
+        plt.ioff()
+        plt.close(fig)
+        print("Cleaned up and exited.")
+
+        # Save the collected data
+        data = {
+            "user_details": user_details,
+            "timestamp": t,
+            "ecg": ecg,
+            "gsr": gsr,
+            "ppg_red": ppg_red,
+            "ppg_ir": ppg_ir,
+            "ppg_green": ppg_green, 
+            "temp": temp
+        }
+        time_now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        file_name = f"{user_details['name']}_{time_now}.json"
+        with open(file_name, "w") as data_file:
+            json.dump(data, data_file)
